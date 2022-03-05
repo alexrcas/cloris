96531348792db46e2c762eee67b6978ff3692e61
arodriguez <arodriguez@avantic.net>
2022-03-05 23:00:57 +0000

    Añadir logger

--- cloris-server/src/app.module.ts
+++ cloris-server/src/app.module.ts
@@ -1,21 +1,25 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module } from '@nestjs/common';
 import { MeasuresController } from './controllers/measures/measures.controller';
 import { MeasuresService } from './services/measures/measures.service';
 import { DatabaseModule } from './modules/database/database.module';
 import { TimestampMiddleware } from './middlewares/timestamp.middleware';
 import { WateringsController } from './controllers/waterings/waterings.controller';
 import { WateringsService } from './services/waterings/waterings.service';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [MeasuresController, WateringsController],
-  providers: [MeasuresService, WateringsService],
+  providers: [MeasuresService, WateringsService, Logger],
 })
 export class AppModule {
   
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(TimestampMiddleware)
-      .forRoutes('measures', 'waterings');
+      .forRoutes('measures', 'waterings')
+      .apply(LoggerMiddleware)
+      .forRoutes('/')
   }
+
 }
