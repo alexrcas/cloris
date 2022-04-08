80161397e27dcd85aecd588324a1c017fc1ad9b4
arodriguez <arodriguez@avantic.net>
2022-04-09 00:02:43 +0100

    Componente quickInfoPanel con datos reales desde el servidor

--- cloris-client/src/components/QuickInfoPanel.tsx
+++ cloris-client/src/components/QuickInfoPanel.tsx
@@ -3,10 +3,11 @@ import { CustomCard } from "./CustomCard"
 import { faTemperatureEmpty } from '@fortawesome/free-solid-svg-icons'
 import { faWater } from '@fortawesome/free-solid-svg-icons'
 import { faCloud } from '@fortawesome/free-solid-svg-icons'
-import { IconDefinition } from "@fortawesome/fontawesome-svg-core"
-import { useEffect, useState } from "react"
+import useFetch from "../hooks/useFetch"
 
 export const QuickInfoPanel = () => {
+
+    const url = 'http://localhost:3000/measures';
 
     interface IenvironmentData {
         id: number,
@@ -16,63 +17,31 @@ export const QuickInfoPanel = () => {
         timestamp: Date
     }
 
-    const [environmentData, setEnvironmentData] = useState<IenvironmentData>()
+    const { data, error } = useFetch<IenvironmentData[]>(url)
 
-    useEffect(() => {
-        fetch('http://localhost:3000/measures')
-            .then(response => response.json())
-            .then(data => console.log(data))
-    }, [])
-    
-    
-
-    const response = {
-        id: "62241adb0371d9f051048717",
-        temperature: 15,
-        airHumidity: 82,
-        terrainHumidity: 43,
-        timestamp: "2022-03-06T02:22:19.351Z"
+    if (!data) {
+        return <p>Cargando...</p>
     }
 
-    const cardAttributes: {title: string, icon: IconDefinition, value: number, unit: string}[] = [
-        {
-            title: 'Temperatura',
-            icon: faTemperatureEmpty,
-            unit: 'ºC',
-            value: response.temperature
-        },
-        {
-            title: 'Humedad del aire',
-            icon: faCloud,
-            unit: '%',
-            value: response.airHumidity
-        },
-        {
-            title: 'Humedad del terreno',
-            icon: faWater,
-            unit: '%',
-            value: response.terrainHumidity
-        }
-    ];
+    const measure: IenvironmentData = data[0];
 
     return (
 
     <Container className="mt-4">
 
         <CardGroup>
-            {
-                cardAttributes.map(cardAttribute => (
-                    <CustomCard {...cardAttribute} key={cardAttribute.title} />
-                ))
-            }
+            <CustomCard title='Temperatura' icon={faTemperatureEmpty} value={measure.temperature} unit='ºC'/>
+            <CustomCard title='Humedad del aire' icon={faCloud} value={measure.airHumidity} unit='%'/>
+            <CustomCard title='Humedad del terreno' icon={faWater} value={measure.terrainHumidity} unit='%'/>
         </CardGroup>
 
         <Row className="mt-2 d-flex">
-            <span className="d-flex justify-content-end fst-italic fw-light">Última sincronización: 04/03/2022 17:42</span>
+            <span className="d-flex justify-content-end fst-italic fw-light">Última sincronización: {measure.timestamp}</span>
         </Row>
 
     </Container>
 
     )
+
 
 }
